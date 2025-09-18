463040c900153d51d20dfaf4999d4fa2c79eed44
SONGLewei <2211305832@qq.com>
2025-09-18 22:06:54 +0200

    new dish/es with flavor, dishController -> dishService-> impl -> dish and flavor mapper sql

--- sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -1,14 +1,28 @@
 package com.sky.service.impl;
 
 import com.sky.dto.DishDTO;
+import com.sky.entity.Dish;
+import com.sky.entity.DishFlavor;
+import com.sky.mapper.DishFlavorMapper;
+import com.sky.mapper.DishMapper;
 import com.sky.service.DishService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Slf4j
 public class DishServiceImpl implements DishService {
+
+    @Autowired
+    private DishMapper dishMapper;
+
+    @Autowired
+    private DishFlavorMapper dishFlavorMapper;
 
     /**
      * new dish and flavor
@@ -16,6 +30,25 @@ public class DishServiceImpl implements DishService {
      */
     @Transactional
     public void saveWithFlavor(DishDTO dishDTO) {
+
+        Dish dish = new Dish();
+
+        BeanUtils.copyProperties(dishDTO,dish);
+        //insert 1 data to the list of dishes
+        dishMapper.insert(dish);
+
+
+        // get the main key from insert
+        Long dishId = dish.getId();
+
+        //insert n data to ...
+        List<DishFlavor> flavors = dishDTO.getFlavors();
+        if(flavors!=null && flavors.size()>0){
+            flavors.forEach(dishFlavor -> {
+                dishFlavor.setDishId(dishId);
+            });
+            dishFlavorMapper.insertBach(flavors);
+        }
 
     }
 }
