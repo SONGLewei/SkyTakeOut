212200a11c6c0599a8910a054efb224c98eb93b7
SONGLewei <2211305832@qq.com>
2025-09-21 18:48:48 +0200

    page query complet and debug

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -9,10 +9,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/admin/dish")
@@ -35,7 +32,11 @@ public class DishController {
         return Result.success();
     }
 
+    @GetMapping("/page")
+    @ApiOperation("dishes page query")
     public Result<PageResult> page(DishPageQueryDTO dishPageQueryDTO){
-        return null;
+        log.info("dishes page query: {}",dishPageQueryDTO);
+        PageResult pageResult = dishService.pageQuery(dishPageQueryDTO);
+        return Result.success(pageResult);
     }
 }
